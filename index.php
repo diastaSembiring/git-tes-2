639c2080f12dd07930118fb37ba831ba629ab3c7
diastaSembiring <natanaeldiasta@gmail.com>
2024-01-27 00:22:35 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remot</title>
+</head>
+<body>
+    <h1>hwloo world</h1>
+</body>
+</html>
